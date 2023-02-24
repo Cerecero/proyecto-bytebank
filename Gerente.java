9384d4c5e060500dfdf75c049d97ded47a68d6d1
Cerecero <ale.rey.cer@outlook.com>
2023-02-24 13:26:57 -0600

    added an extends to Gerente from Funcionario

--- Gerente.java
+++ Gerente.java
@@ -8,32 +8,13 @@ package proyecto.bytebank;
  *
  * @author alejandro
  */
-public class Gerente {
-    private String nombre;
-    private String documento;
-    private double salario;
-
-    public String getNombre() {
-        return nombre;
+public class Gerente extends Funcionario{
+    private String clave;
+    public void setClave(String clave){
+        this.clave = clave;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
+    
+    public boolean iniciarSesion(String clave){
+        return clave == "ClaveCorrecta";
     }
 }
