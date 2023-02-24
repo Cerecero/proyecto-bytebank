7203749f693713c017ee3983f02f202779c66b0a
Cerecero <ale.rey.cer@outlook.com>
2023-02-24 13:12:23 -0600

    added a new class called Gerente

---
+++ Gerente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.bytebank;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Gerente {
+    private String nombre;
+    private String documento;
+    private double salario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
