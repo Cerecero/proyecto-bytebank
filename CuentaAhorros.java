548f5e2455fe910ed189a6b4f4b6720602531e39
Cerecero <ale.rey.cer@outlook.com>
2023-02-27 19:00:12 -0600

    added a new file CuentaAhorros

---
+++ CuentaAhorros.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.bytebank;
+
+/**
+ *
+ * @author alejandro
+ */
+public class CuentaAhorros extends Cuenta {
+    
+    public CuentaAhorros(int agencia) {
+        super(agencia);
+    }
+    
+}
