1479a5a8e2538a6fc696a8270f58849042bef43e
Cerecero <ale.rey.cer@outlook.com>
2023-02-27 18:59:50 -0600

    added a new file CuentaCorriente

---
+++ CuentaCorriente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.bytebank;
+
+/**
+ *
+ * @author alejandro
+ */
+public class CuentaCorriente extends Cuenta{
+    
+    public CuentaCorriente(int agencia){
+        super(agencia);
+    }
+    
+}
