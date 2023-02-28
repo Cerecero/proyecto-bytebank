2ea77ee262f336176685aa7ca189fa8049dae115
Cerecero <ale.rey.cer@outlook.com>
2023-02-27 18:04:22 -0600

    added a new file TestReferencias

---
+++ TestReferencias.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.bytebank;
+
+/**
+ *
+ * @author alejandro
+ */
+public class TestReferencias {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNombre("Diego");
+        
+        Gerente gerente = new Gerente();
+        gerente.setNombre("JImena");
+        
+        funcionario.setSalario(2000);
+        gerente.setSalario(10000);
+    }
+}
