d002b4c736a9a4bcc505599e10faabf57883f10e
Cerecero <ale.rey.cer@outlook.com>
2023-02-27 18:36:05 -0600

    removed repeating methods

--- ControlBonificacion.java
+++ ControlBonificacion.java
@@ -17,14 +17,4 @@ public class ControlBonificacion {
         System.out.println("Calculo actual: " + this.suma);
         return this.suma;
     }
-    public double registrarSalario(Gerente gerente){
-        this.suma = gerente.getBonificacion() + this.suma;
-        System.out.println("Calculo actual: " + this.suma);
-        return this.suma;
-    }
-    public double registrarSalario(Contador contador){
-        this.suma = contador.getBonificacion() + this.suma;
-        System.out.println("Calculo actual: " + this.suma);
-        return this.suma;
-    }
 }
